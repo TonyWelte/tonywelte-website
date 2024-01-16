e5f1b080a9f1bae2f31ab806dc3b72ba9f2cf460
Anthony Welte <tony.welte@gmail.com>
2024-01-16 07:51:14 +0100

    First React page

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Anthony Welte</h1>
+      <p>Robotics Engineer, PhD</p>
+    </header>
+  );
+};
+
+export default Header;
